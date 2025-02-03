9b40d7c980bee8782c67a4f8eeb5cda30698f92d
abril <abril_al@yahoo.com>
2025-02-03 06:43:15 -0800

    3.3 String spelled by genome and 3.2 edge case fix

--- hw4.py
+++ hw4.py
@@ -5,9 +5,16 @@ from typing import List, Dict, Iterable
 
 def kmer_composition(text: str, k: int) -> Iterable[str]:
     """Forms the k-mer composition of a string."""
-    return [ text[i:i+k] for i in range(0,len(text)-k)] 
+    return [ text[i:i+k] for i in range(0,len(text)-k+1)] 
 
 #3.3. String Reconstruction as a Walk in the Overlap Graph: Q3, Q10
+
+    # Q3: String Spelled by a Genome Path Problem.
+def genome_path(path: List[str]) -> str:
+    """Forms the genome path formed by a collection of patterns."""
+    return path[0] + ''.join([elem[-1] for elem in path[1:]])
+#print(genome_path(['ACCGA', 'CCGAA', 'CGAAG', 'GAAGC', 'AAGCT'])) # should return ACCGAAGCT
+
 
 #3.4. Another Graph for String Reconstruction: Q6
 
