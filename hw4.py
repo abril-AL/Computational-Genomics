314c68b1a649416997ebd18f8d44f6bab843f79e
abril <abril_al@yahoo.com>
2025-02-03 06:28:17 -0800

    3.2 kmer composition

---
+++ hw4.py
@@ -0,0 +1,16 @@
+#3.2. The String Reconstruction Problem: Q3
+
+import sys
+from typing import List, Dict, Iterable
+
+def kmer_composition(text: str, k: int) -> Iterable[str]:
+    """Forms the k-mer composition of a string."""
+    return [ text[i:i+k] for i in range(0,len(text)-k)] 
+
+#3.3. String Reconstruction as a Walk in the Overlap Graph: Q3, Q10
+
+#3.4. Another Graph for String Reconstruction: Q6
+
+#3.5. Walking in the de Bruijn Graph: Q8
+
+#3.8. From Euler's Theorem to an Algorithm for Finding Eulerian Cycles: Q2, Q6, Q7
