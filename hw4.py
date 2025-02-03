8db060d20a3433583db28a0001277b0859bd99cc
abril <abril_al@yahoo.com>
2025-02-03 14:53:52 -0800

    finally... Eulerian path problem

--- hw4.py
+++ hw4.py
@@ -99,6 +99,7 @@ def eulerian_cycle(g: Dict[int, List[int]]) -> Iterable[int]:
     #print("G:",graph)
     
     stack = [next(iter(graph))]  # Start with the first vertex
+    print("s:",stack)
     cycle = []
 
     while stack:
@@ -126,117 +127,71 @@ graph = {
 
 #print(eulerian_cycle(graph))
 
-#0: 3
-#1: 0
-#2: 1 6
-#3: 2
-#4: 2
-#5: 4
-#6: 5 8
-#7: 9
-#8: 7
-#9: 6
-# 0 3 2 6 8 7 9 6 5 4 2 1 0
-
     # Q6: Eulerian Path Problem.
 from typing import List, Dict, Iterable
 from collections import defaultdict
 import random
 from copy import deepcopy
 from collections import Counter
-'''
+
+g2 = {
+    0: [2],
+    1: [3],
+    2: [1],
+    3: [0, 4],
+    6: [3, 7],
+    7: [8],
+    8: [9],
+    9: [6]
+}
+# 6 7 8 9 6 3 0 2 1 3 4
+
 def eulerian_path(adj_dict):
+    in_deg = Counter()
+    out_deg = Counter()
 
-    # Adjacency dictionary with 'false' edge added to make a cycle
-    cycle_d = deepcopy(adj_dict)
+    for node in adj_dict:
+        out_deg[node] += len(adj_dict[node])
+        for neighbor in adj_dict[node]:
+            in_deg[neighbor] += 1
+    
+    #print("Out:",out_deg)
+    #print("In:",in_deg)
 
-    # Initializes Counter that will track which nodes are unbalanced,
-    # which gives us the start and finish
-    find_ends = Counter()
+    start_node = None
+    end_node = None
+    for node in set(list(in_deg.keys()) + list(out_deg.keys())):
+        in_d = in_deg.get(node, 0)
+        out_d = out_deg.get(node, 0)
+        if in_d - out_d == -1:
+            start_node = node
+        elif in_d - out_d == 1:
+            end_node = node
+        elif in_d - out_d != 0:
+            raise Exception("Graph is not Eulerian")
 
-    # Go through each node, using the counter find_ends to count how many nodes
-    # are adjacent to the current node and subtract 1 for each of those adj
-    for row in cycle_d.items():
-        dir_out = row[0]
-        dir_ins = row[1]
+    #print("Start node:", start_node) # 6
+    #print("End node:", end_node) # 4
 
-        for ins in dir_ins:
-            find_ends[ins] -= 1
-            find_ends[dir_out] += 1
+    # construct Eulerian cycle 
+    graph = {u: list(adj_dict[u]) for u in adj_dict}
+    for node in in_deg.keys():
+        if node not in graph:
+            graph[node] = [] 
+    
+    stack = [start_node]  # Start with the start vertex
+    path = []
 
-    # The most_common function return a list of elements and their counts
-    # from the most common to the least.
-    # The starting node (V0) will be most common, as there is 1 less node
-    # adjacent to it, while the ending node will have 1 less adjacency
-    start = find_ends.most_common()[0][0]
-    end = find_ends.most_common()[-1][0]
+    while stack:
+        #print('Stack:', stack)
+        #print('Current Node:', stack[-1])
+        if stack[-1] not in graph:
+            raise KeyError(f"Node {stack[-1]} is missing from graph")
+        #print('Edges:', graph[stack[-1]])  
+        while graph[stack[-1]]:  # This is where the KeyError likely occurs
+            stack.append(graph[stack[-1]].pop())
+        path.append(stack.pop())
 
-    # Create a 'false' edge between the end node and the start node so as to
-    # enable the algorithm for Eulerian cycles to function
-    try:
-        cycle_d[end].append(start)
-    except KeyError:
-        cycle_d[end] = [start]
+    return path[::-1]
 
-    cycle = eulerian_cycle(cycle_d)
-
-    # Eulerian cycle needs to be re-oriented to begin with the "true" starteulerian_
-    for i, n in enumerate(cycle):
-        if n == end:
-            if cycle[i+1] == start:
-                break_point = i
-                break
-
-    cycle =  cycle[break_point+1:] + cycle[1:break_point+1]
-
-    return cycle
-
-def eulerian_cycle(adj_dict):
-
-    # Dictionary that tracks remaining edges (those not yet taken),
-    # initialized as the input adjacency dict
-    remain_d = deepcopy(adj_dict)
-
-    # Randomly select a starting node
-    node = random.choice(range(len(adj_dict)))
-
-    # Create list to track Eulerian cycle
-    cycle= [node]
-
-    # Continue as long as there are edges that remain untaken
-    while len(remain_d) > 0:
-
-        # Check if any adjacencies remain for the node and if so, how many
-        value = remain_d.get(node)
-
-        # If the node has no unused edges, we must be back at V0 (since the
-        # graph is balanced), but we also know there are remaining edges out
-        # there. We need to expand the circle until it encompasses all nodes.
-        if value == None:
-
-            # To do so, iterate through the current "cycle" until we find a
-            # node with an unused edge. Make that the new V0.
-            for i, n in enumerate(cycle):
-                if remain_d.get(n) > 0:
-                    node = n
-                    cycle = cycle[i:]+cycle[1:i+1]
-                    break
-
-        # If the node has a single unused edge, simply use it to continue the
-        # cycle by adding it to the list 'cycle' and removing it from the dict
-        elif len(value) == 1:
-            node = remain_d.pop(node)[0]
-            cycle.append(node)
-
-        # If the node has multiple unused edges, randomly select one to add to
-        # the cycle and remove it out from the node's list of adjacencies.
-        elif len(value) > 1:
-            random_i = random.randrange(len(value))
-            pos_nodes = remain_d[node]
-            new_node = pos_nodes.pop(random_i)
-            remain_d[node] = pos_nodes
-            node = new_node
-            cycle.append(node)
-
-    return cycle
-'''
+print(eulerian_path(g2))
