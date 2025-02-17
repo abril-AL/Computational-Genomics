df1674bf3c91f9c111c44cb486313a5e995daede
abril <abril_al@yahoo.com>
2025-02-16 16:58:25 -0800

    farthest first traversal

---
+++ hw5.py
@@ -0,0 +1,26 @@
+# Homework 5 - Clustering Algorithms (Ch. 8)
+#
+# 8.6. Farther First Traversal: Q2* (recommended)
+# 8.7. k-Means Clustering: Q3
+# 8.8. The Lloyd Algorithm: Q3
+# 8.14. Hierarchical Clustering: Q7
+
+import sys
+from typing import List, Dict, Iterable, Tuple
+import math
+
+# Farthest First Traversal
+'''
+FarthestFirstTraversal(Data, k) 
+    Centers ← the set consisting of a single randomly chosen point from Data
+    while |Centers| < k 
+        DataPoint ← the point in Data maximizing d(DataPoint, Centers) 
+        add DataPoint to Centers 
+    return Centers '''
+def farthest_first_traversal(k: int, m: int, data: List[Tuple[float, ...]]) -> List[Tuple[float, ...]]:    
+    centers = set()
+    centers.add(data[0])
+    while centers.__len__() < k:
+        dataPoint = max(data, key=lambda point: min(math.dist(point, center) for center in centers))
+        centers.add(dataPoint)
+    return centers
