3ba4d9385e79857b400ac2f8b334d6940d0dec12
abril <abril_al@yahoo.com>
2025-02-24 11:57:47 -0800

    hw6 (ch2) init

---
+++ hw6.py
@@ -0,0 +1,9 @@
+#    HW6 – Chapter 2 (due Tue Feb 25th, 2025 at 12pm)       #
+#############################################################
+# 2.2. Motif Finding Is More Difficult Than You Think: Q8 
+# 2.5. Greedy Motif Search: Q2, Q3 
+# 2.6. Motif Finding Meets Oliver Cromwell Q9* (recommended) 
+# 2.7. Randomized Motif Search: Q5* (recommended) 
+# 2.9. Gibbs Sampling: Q11* (recommended)
+
+
