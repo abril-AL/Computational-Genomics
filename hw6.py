faf1f58e07d9716e9bddeeb7a8ae91d262fd1bcd
abril <abril_al@yahoo.com>
2025-02-24 14:55:49 -0800

    greedy motif search problem

--- hw6.py
+++ hw6.py
@@ -50,3 +50,80 @@ def profile_most_probable_kmer(text: str, k: int,
         probs.append(prob)
     max_prob_index = probs.index(max(probs))
     return text[max_prob_index:max_prob_index + k]
+
+'''GreedyMotifSearch(Dna, k, t)
+    BestMotifs ← motif matrix formed by first k-mers in each string from Dna
+    for each k-mer Motif in the first string from Dna
+        Motif1 ← Motif
+        for i = 2 to t
+            form Profile from motifs Motif1, …, Motifi - 1
+            Motifi ← Profile-most probable k-mer in the i-th string in Dna
+        Motifs ← (Motif1, …, Motift)
+        if Score(Motifs) < Score(BestMotifs)
+            BestMotifs ← Motifs
+    return BestMotifs'''
+
+# Greedy Motif Search
+def greedy_motif_search(dna: list[str], k: int, t: int) -> list[str]:
+    BestMotifs = []
+    for i in range(0, t):
+        BestMotifs.append(dna[i][0:k])
+    n = len(dna[0])
+    for i in range(n-k+1):
+        Motifs = []
+        Motifs.append(dna[0][i:i+k])
+        for j in range(1, t):
+            P = build_profile(Motifs[0:j])
+            Motifs.append(ProfileMostProbablePattern(dna[j], k, P))
+        if Score(Motifs) < Score(BestMotifs):
+            BestMotifs = Motifs
+    return BestMotifs
+
+def build_profile(Motifs):
+    count = {} # initializing the count dictionary
+    profile = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    ## divide the number of motif strings to get frequency
+    for letter in count.keys():
+        profile[letter] = [x/ float(t) for x in count[letter]]
+    return profile
+
+def Score(motifs):
+    """Calculates the score of a motif matrix based on the number of non-consensus nucleotides."""
+    k = len(motifs[0])
+    consensus = ""
+    score = 0
+    
+    for i in range(k):
+        column = [motif[i] for motif in motifs]
+        most_common = max(set(column), key=column.count)
+        consensus += most_common
+        score += sum(1 for char in column if char != most_common)
+    
+    return score
+
+def ProfileMostProbablePattern(Text, k, Profile):
+    p_dict = {}
+    for i in range(len(Text)- k +1):
+        p = Pr(Text[i: i+k], Profile)
+        p_dict[i] = p
+    m = max(p_dict.values())
+    keys = [k for k,v in p_dict.items() if v == m]
+    ind = keys[0]
+    return Text[ind: ind +k]
+
+def Pr(Text, Profile):
+    p = 1
+    for i in range(len(Text)):
+        p = p * Profile[Text[i]][i]
+    return p
